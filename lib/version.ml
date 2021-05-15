7dc3b00243a7b73b684d6582ce2787c92010784a
TitiFix <github.titifix@gmail.com>
2021-05-15 19:40:48 +0200

    mode -proc x (0 ou 1) préliminaire et -bufsize en Windows seulement

--- lib/version.ml
+++ lib/version.ml
@@ -1,6 +1,6 @@
 (* Copyright (c) 1998-2007 INRIA *)
 
-let txt = "7.1.0.21051518_TitiFix_fixwserver2" (* from TitiFix_fixwserver2 commit 0c171ec0 May_11_08:22:12_2021 *)
+let txt = "7.1.0.21051519_TitiFix_fixwserver2" (* from TitiFix_fixwserver2 commit e9680a49 May_15_19:01:47_2021 *)
 
 let available_languages =
   ["af"; "bg"; "br"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
