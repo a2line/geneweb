3c37cc9eaf31d5b4a5ebd2caea33f4ab8b2b0395
Henri83 <henri.gouraud@laposte.net>
2025-02-10 09:19:09 +0100

    fix broken italic and bold in wiki.ml

--- lib/wiki.ml
+++ lib/wiki.ml
@@ -242,7 +242,7 @@ let syntax_links conf wi s =
           in
           loop quot_lev (pos + 1) j (Buff.mstore len t)
       | NotesLinks.WLnone (j, none_s) ->
-          loop quot_lev pos j (Buff.mstore len none_s)
+          loop quot_lev pos (i + 1) (Buff.store len s.[i])
   in
   loop 0 1 0 0
 
